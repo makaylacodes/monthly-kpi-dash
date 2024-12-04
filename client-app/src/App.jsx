6ed74e7e8af3d977dc049fb15c2dc411109f185a
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-12-03 17:05:21 -0800

    Created a react app with vite. Moved all the default code to file, Original.jsx and turned it into components. Will remove later

---
+++ client-app/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Card } from './Original';
+import { Logos } from './Original';
+import { Subtitle } from './Original';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <>
+      <Logos />
+      <h1>Quarterly KPI Dashboard</h1>
+      < Card />
+      <Subtitle />
+    </>
+  )
+}
+
+export default App
---
+++ client-app/src/Original.jsx
@@ -0,0 +1,35 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { useState } from 'react';
+
+export const Logos = () => {
+      return ( <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>);
+  }
+
+  export const Card = () => {
+    
+    const [count, setCount] = useState(0);
+    return (
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+            </button>
+            <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+      </div>);
+  }
+
+ export const Subtitle = () => {
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+        </p>
+  }
+
